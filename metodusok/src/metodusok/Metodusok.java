c6a5c320a1bdfb41404fe3f9a90f6f3c28b7887e
enikonagy2005 <83341636+enikonagy2005@users.noreply.github.com>
2022-11-04 12:48:35 +0100

    1104

---
+++ metodusok/src/metodusok/Metodusok.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodusok;
+
+/**
+ *
+ * @author user3
+ */
+public class Metodusok {
+
+    //metódusok és osztályok helye
+    public static double atlag(int szam1, int szam2) {
+        
+    System.out.println("Adj meg egy tetszőleges számot: ");
+    System.out.println("Adj meg mégegy tetszőlges számot: ");
+     
+    double atl = (szam1 + szam2) / 2.0;
+    return atl;
+   
+       
+    }
+
+    public static void main(String[] args) {
+        // fejelc
+        System.out.println("**********************");
+        System.out.println("*       Feladat      *");
+        System.out.println("**********************");
+        
+        
+        // kérjünk be a felhasználotól 2 számot. A kisebbik legyen egy tartomány alsó határa a nagyobbik a felső.
+        // a, Generáltassunk a tartományból egy random számot
+        // b, Hozzunk létre a függvény segítségével egy 20 elemű tömböt
+        System.out.println("Adjon meg egy számot: ");
+        System.out.println("Adjon meg mégegy számot: ");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+}
